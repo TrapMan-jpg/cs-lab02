6e60239a760a660f3a9bd58f8cc7fe866f0202db
alice (MartynovAV) <alice@example.com>
2021-03-15 17:01:22 +0300

    code: нахождение максимума

--- main.cpp
+++ main.cpp
@@ -8,8 +8,9 @@ int main()
 int a, b;
 cin >> a >> b;
 cout << "A + B = " << a + b << '\n';
-cout << "A + B = " << a + b << '\n'
-     << "A - B = " << a - b << '\n';
-
+cout << "A / B = " << a + b << '\n'
+     << "A - B = " << a - b << '\n'
+     << "A * B = " << a * b << '\n';
+cout << "max(A,B)="<<max(a,b);
     return 0;
 }
